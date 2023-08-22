ac8a273be2a3fd296f2a8d03939ffa6bddb46c62
Gunho <104866923+koreanMike513@users.noreply.github.com>
2023-08-22 10:43:26 +0900

    Time: 23 ms (95.89%), Space: 42.6 MB (97.94%) - LeetHub

---
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int triangleNumber(int[] nums) {
+        
+        Arrays.sort(nums);
+
+        int ans = 0;
+
+        for (int i = 2; i < nums.length; i++) {
+
+            int left = 0, right = i - 1;
+
+            while (left < right) {
+                
+                if (nums[left] + nums[right] > nums[i]) {
+                    ans += right - left;
+                    right--;
+                }
+
+                else {
+                    left++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
