9344dc6540c4c580ffcf2324e5d932869b0bacf4
Gunho <104866923+koreanMike513@users.noreply.github.com>
2023-11-23 12:56:53 +0000

    Time: 142 ms (65.67%), Space: 40.1 MB (92.01%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,38 @@
+class Solution {
+    private boolean[][] visited;
+    
+    public boolean exist(char[][] board, String word) {
+	    int m = board.length, n = board[0].length;
+        visited = new boolean[m][n];
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0) && 
+                    backtrack(board, i, j, 0, word)) return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    public boolean backtrack(char[][] board, int i, int j, int index, String word) {
+        if (index == word.length()) return true;
+        
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(index) || visited[i][j]) {
+            return false;
+        }
+            
+        visited[i][j] = true;
+        
+        boolean found = (
+            backtrack(board, i + 1, j, index + 1, word) ||
+            backtrack(board, i - 1, j, index + 1, word) ||
+            backtrack(board, i, j + 1, index + 1, word) ||
+            backtrack(board, i, j - 1, index + 1, word)
+        );
+            
+        visited[i][j] = false;
+		
+        return found;
+    }
+}
