910203a260abb12357834caebbe3387be420ec11
Gunho <104866923+koreanMike513@users.noreply.github.com>
2023-12-24 19:35:18 +0000

    Time: 2 ms (98.18%), Space: 41.4 MB (10.18%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (int num : map.values()) {
+            if (set.contains(num)) {
+                return false;
+            }
+
+            set.add(num);
+        }
+
+        return true;
+    }
+}
