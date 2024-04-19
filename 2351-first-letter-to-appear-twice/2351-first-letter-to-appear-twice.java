5eb7d09cb42d7ad4a7f42c43e8254e87a293f3a0
Gunho <104866923+koreanMike513@users.noreply.github.com>
2024-04-19 12:23:04 +0100

    Time: 0 ms (100.00%), Space: 41.3 MB (59.04%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+        int appear = 0, mask = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            mask = 1 << (s.charAt(i) - 'a');
+
+            if ((appear & mask) != 0) {
+                return s.charAt(i);
+            }
+
+            appear |= mask;
+        }
+        
+        return '-';
+    }
+}
