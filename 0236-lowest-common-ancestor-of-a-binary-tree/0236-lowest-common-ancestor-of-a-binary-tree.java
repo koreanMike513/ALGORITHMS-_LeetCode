a2e7ef52672c71df0e9afea54edcc9d40719bedc
Gunho <104866923+koreanMike513@users.noreply.github.com>
2024-10-10 09:43:08 +0900

    Time: 9 ms (15.24%), Space: 46.4 MB (5.10%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,12 +1,38 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null) return null;
-        if (root == p || root == q) return root;
+        Map<Integer, TreeNode> parents = new HashMap<>();
+        Map<Integer, Integer> depth = new HashMap<>(); 
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        DFS(parents, depth, root, null, 1);
 
-        if (left != null && right != null) return root;
-        return left == null ? right : left;
+        return LCA(parents, depth, p, q);
+    }
+
+    private void DFS(Map<Integer, TreeNode> parents, Map<Integer, Integer> depth, TreeNode node, TreeNode parent, int currentDepth) {  
+        if (node == null || parents.containsKey(node.val)) {
+            return;
+        }
+
+        parents.put(node.val, parent);
+        depth.put(node.val, currentDepth);
+
+        DFS(parents, depth, node.left, node, currentDepth + 1);
+        DFS(parents, depth, node.right, node, currentDepth + 1);
+    }
+
+    private TreeNode LCA(Map<Integer, TreeNode> parents, Map<Integer, Integer> depth, TreeNode p, TreeNode q) {
+        if (p == q) {
+            return p;
+        }
+
+        if (depth.get(p.val) < depth.get(q.val)) {
+            return LCA(parents, depth, p, parents.get(q.val));
+        }
+
+        else if (depth.get(p.val) > depth.get(q.val)) {
+            return LCA(parents, depth, parents.get(p.val), q);
+        }
+
+        return LCA(parents, depth, parents.get(p.val), parents.get(q.val));
     }
 }
