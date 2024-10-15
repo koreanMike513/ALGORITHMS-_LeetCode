344d923ac835b1deebbdc86f72d871c4e0bfa519
Gunho <104866923+koreanMike513@users.noreply.github.com>
2024-10-15 20:57:56 +0900

    Time: 16 ms (58.83%), Space: 44.2 MB (73.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,24 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        
         int[] dp = new int[amount + 1];
+
         Arrays.fill(dp, 10001);
+        dp[0] = 0;
 
-        for (int i = 0; i < coins.length; i++) {
-            if (amount + 1 > coins[i]) {
-                dp[coins[i]] = 1;
-            }
-        }
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i == coin) {
+                    dp[i] = 1;
+                }
 
-        for (int i = 0; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0 && dp[i - coins[j]] > 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                else if (i - coin >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
         }
 
-        return (dp[amount] != 10001) ? dp[amount] : -1;
+        return dp[amount] != 10001 ? dp[amount] : -1;
     }
 }
