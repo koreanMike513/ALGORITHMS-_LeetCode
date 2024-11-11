37666b3facd68284143f85340317478a4918e963
Gunho <104866923+koreanMike513@users.noreply.github.com>
2024-11-12 00:46:02 +0900

    Time: 2 ms (72.11%), Space: 42.8 MB (13.80%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,33 +1,16 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         Queue<int[]> qu = new LinkedList<>();
-        int n = grid.length, m = grid[0].length, total = 0, ans = 0;
-        int[][] directions = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+        int[][] directions = new int[][]{ { 0, 1 }, { 0, - 1}, { 1, 0 }, { - 1, 0 } };
+        int seconds = -1, n = grid.length, m = grid[0].length;
+        boolean[][] vis = new boolean[n][m];
+        int fresh = findRottenOrangesAndCountFreshOranges(grid, qu);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    qu.add(new int[] { i, j });
-                }
-
-                else if (grid[i][j] == 1) {
-                    total++;
-                }
-            }
-        }
-
-        if (total == 0) {
+        if (fresh == 0)
             return 0;
-        }
 
         while (!qu.isEmpty()) {
-            if (total == 0) {
-                return ans;
-            }
-
             int size = qu.size();
-            ans++;
-
             for (int i = 0; i < size; i++) {
                 int[] current = qu.poll();
 
@@ -35,19 +18,34 @@ class Solution {
                     int x = current[0] + direction[0];
                     int y = current[1] + direction[1];
 
-                    if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] != 1) {
+                    if (x < 0 || y < 0 || x >= n || y >= m || vis[x][y] || grid[x][y] != 1)
                         continue;
-                    }
-
-                    if (grid[x][y] == 1) {
-                        grid[x][y] = 2;
-                        qu.add(new int[] { x, y });
-                        total--;
-                    }
+                    
+                    qu.add(new int[]{ x, y });
+                    fresh--;
+                    vis[x][y] = true;
                 }
+            }
+
+            seconds++;
+        }
+
+        return (fresh == 0) ? seconds : -1;
+    }
+
+    private int findRottenOrangesAndCountFreshOranges(int[][] grid, Queue<int[]> qu) {
+        int fresh = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2)
+                    qu.add(new int[]{ i, j });
+
+                else if (grid[i][j] == 1)
+                    fresh++;
             }
         }
 
-        return -1;
+        return fresh;
     }
 }
