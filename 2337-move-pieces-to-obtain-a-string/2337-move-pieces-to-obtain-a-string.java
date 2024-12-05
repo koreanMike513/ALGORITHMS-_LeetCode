75709c873a28448a855e5b55d1b7c401c6a6b703
Gunho <104866923+koreanMike513@users.noreply.github.com>
2024-12-05 16:08:58 +0900

    Time: 15 ms (66.47%), Space: 45.9 MB (23.44%) - LeetHub

--- 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.java
@@ -1,25 +1,29 @@
 class Solution {
     public boolean canChange(String start, String target) {
-        int sp = 0, tp = 0, sl = start.length(), tl = target.length();
+        int n = start.length(), startIdx = 0, targetIdx = 0;
 
-        if (sl != tl) return false;
-        
-        while (sp < sl || tp < tl) {
-            while (sp < sl && start.charAt(sp) == '_') { sp++; }
-
-            while (tp < tl && target.charAt(tp) == '_') { tp++; }
-
-            char c1 = (sp < sl) ? start.charAt(sp) : '_';
-            char c2 = (tp < tl) ? target.charAt(tp) : '_';
-            
-            if (((c1 != c2)) || (c1 == 'R' && sp > tp) || (c1 == 'L' && sp < tp)) {
-                return false;
+        while (startIdx < n || targetIdx < n) {
+            while (startIdx < n && start.charAt(startIdx) == '_') {
+                startIdx++;
             }
 
-            sp++; tp++;
+            while (targetIdx < n && target.charAt(targetIdx) == '_') {
+                targetIdx++;
+            }   
+
+            if (startIdx == n || targetIdx == n) {
+                return startIdx == targetIdx;
+            }
+
+            if (start.charAt(startIdx) != target.charAt(targetIdx) || 
+               (start.charAt(startIdx) == 'R' && startIdx > targetIdx) ||
+               (start.charAt(startIdx) == 'L' && startIdx < targetIdx)
+            )
+                return false;
+
+            startIdx++;
+            targetIdx++;
         }
-        
-        if (sp < sl || tp < tl) return false;
 
         return true;
     }
