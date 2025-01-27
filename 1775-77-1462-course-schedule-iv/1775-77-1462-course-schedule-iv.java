e725a75c5e84f5c6efd460abe4a3272fcd717d24
Gunho <104866923+koreanMike513@users.noreply.github.com>
2025-01-27 13:50:31 +0000

    Time: 71 ms, Memory: 48.1 MB - LeetHub

---
+++ 1775-77-1462-course-schedule-iv/1775-77-1462-course-schedule-iv.java
@@ -0,0 +1,59 @@
+public class Solution {
+
+    public List<Boolean> checkIfPrerequisite(
+        int numCourses,
+        int[][] prerequisites,
+        int[][] queries
+    ) {
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+        int[] indegree = new int[numCourses];
+
+        for (int[] edge : prerequisites) {
+            adjList
+                .computeIfAbsent(edge[0], k -> new ArrayList<>())
+                .add(edge[1]);
+            indegree[edge[1]]++;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        Map<Integer, Set<Integer>> nodePrerequisites = new HashMap<>();
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int adj : adjList.getOrDefault(node, new ArrayList<>())) {
+                nodePrerequisites
+                    .computeIfAbsent(adj, k -> new HashSet<>())
+                    .add(node);
+                for (int prereq : nodePrerequisites.getOrDefault(
+                    node,
+                    new HashSet<>()
+                )) {
+                    nodePrerequisites.get(adj).add(prereq);
+                }
+
+                indegree[adj]--;
+                if (indegree[adj] == 0) {
+                    q.offer(adj);
+                }
+            }
+        }
+
+        List<Boolean> answer = new ArrayList<>();
+        for (int[] query : queries) {
+            answer.add(
+                nodePrerequisites
+                    .getOrDefault(query[1], new HashSet<>())
+                    .contains(query[0])
+            );
+        }
+
+        return answer;
+    }
+}
