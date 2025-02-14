6bb565506623bab4df06932770778cdcc1d229bc
Gunho <104866923+koreanMike513@users.noreply.github.com>
2025-02-14 14:20:49 +0000

    Time: 14 ms, Memory: 68.6 MB - LeetHub

---
+++ 1870-92-1352-product-of-the-last-k-numbers/1870-92-1352-product-of-the-last-k-numbers.java
@@ -0,0 +1,33 @@
+class ProductOfNumbers {
+
+    private ArrayList<Integer> prefixProduct = new ArrayList<>();
+    private int size = 0;
+
+    public ProductOfNumbers() {
+        this.prefixProduct.add(1);
+        this.size = 0;
+    }
+
+    public void add(int num) {
+        if (num == 0) {
+            this.prefixProduct = new ArrayList<Integer>();
+            this.prefixProduct.add(1);
+            this.size = 0;
+        } 
+        
+        else {
+            this.prefixProduct.add(this.prefixProduct.get(size) * num);
+            this.size++;
+        }
+    }
+
+    public int getProduct(int k) {
+        if (k > this.size) 
+            return 0;
+
+        return (
+            this.prefixProduct.get(this.size) /
+            this.prefixProduct.get(this.size - k)
+        );
+    }
+}
